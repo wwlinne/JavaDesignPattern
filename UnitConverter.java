f657ec397556c3a7373323a093600c807795adf3
wwlinne <87803550+wwlinne@users.noreply.github.com>
2023-06-03 11:54:37 -0400

    Update UnitConverter.java

--- UnitConverter.java
+++ UnitConverter.java
@@ -1,13 +1,10 @@
 /*
-File: unitConverter.java
-Student Name: Guohui Wan
-Student Number: 041059351
-Course & Section: 23S_CST8288_021
-Professor: Siju Philip
+File: CFconverter.java
+Name: Guohui Wan
 Declaration: This is my own original work and is free from Plagiarism.
-Modified: 2023, May 18th
+Modified: 2023, May 17th
 Date: 2023, May 16th
-Description: An interface class for converter
+Description: A class to convert Celsius to Fahrenheit 
  */
 package pkgUnitConverter;
 

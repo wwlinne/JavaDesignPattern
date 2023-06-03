24c102a7eea0c42ec7de0746c643c1d14b27fe29
wwlinne <87803550+wwlinne@users.noreply.github.com>
2023-06-03 11:54:59 -0400

    Update WKconverter.java

--- WKconverter.java
+++ WKconverter.java
@@ -1,13 +1,10 @@
 /*
-File: WKconverter.java
-Student Name: Guohui Wan
-Student Number: 041059351
-Course & Section: 23S_CST8288_021
-Professor: Siju Philip
+File: CFconverter.java
+Name: Guohui Wan
 Declaration: This is my own original work and is free from Plagiarism.
-Modified: 2023, May 16th
+Modified: 2023, May 17th
 Date: 2023, May 16th
-Description: A class to convert watt to kilowatt
+Description: A class to convert Celsius to Fahrenheit 
  */
 package pkgUnitConverter;
 
